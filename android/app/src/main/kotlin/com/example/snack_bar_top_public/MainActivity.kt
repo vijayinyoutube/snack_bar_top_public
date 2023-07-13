6ab78b40a6ed373dfb2b06cd3519a1e727bf50df
vijayinyoutube <vijaythevirtue@gmail.com>
2023-07-13 18:24:23 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/snack_bar_top_public/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.snack_bar_top_public
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Presentation/Screens/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SnackBar Demo'),
+        backgroundColor: Colors.pink.withOpacity(0.7),
+      ),
+      body: Center(
+          child: ElevatedButton(
+        onPressed: () => showSnackBar(context),
+        child: const Text('Show Snackbar'),
+      )),
+    );
+  }
+
+  showSnackBar(context) {
+    SnackBar snackBar = SnackBar(
+      content: const Text('Yay! A SnackBar at the top!',
+          style: TextStyle(fontSize: 20)),
+      backgroundColor: Colors.indigo,
+      dismissDirection: DismissDirection.up,
+      behavior: SnackBarBehavior.floating,
+      margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).size.height - 150,
+          //top:150
+          left: 10,
+          right: 10),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+  
+}
